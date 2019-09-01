1465647c08f98a31db9d3fe48e36ab3680f5aad8
Numan Ahmed <39140801+mdnmnahmed@users.noreply.github.com>
2019-09-01 20:19:49 +0530

    Add files via upload

---
+++ Front Page/index.php
@@ -0,0 +1,101 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	<title>Learn HTML n JS</title>
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no"/>
+	<link rel="stylesheet" href="css/main.css"/>
+	<link rel="stylesheet" href="css/index.css"/>
+	<link rel="icon" href="images\N.jpg">
+</head>
+<body bgcolor="red">
+
+         <!-- Wrapper -->
+	   <div id="wrapper">
+
+	      <!-- Main -->
+		<div id="main">
+	    <div class="inner">
+
+			  <!-- Header -->
+		   <header id="header">
+			<a href="index.html" class="logo"><strong>Coding Test</strong></a>
+       	    <!--<ul class="icons">
+		      <li><a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
+			  <li><a href="#" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
+			  <li><a href="#" class="icon fa-whatsapp"><span class="label">Facebook</span></a></li>
+			  <li><a href="#" class="icon fa-snapchat-ghost"><span class="label">Snapchat</span></a></li>
+			  <li><a href="#" class="icon fa-instagram"><span class="label">Instagram</span></a></li>
+			  <li><a href="#" class="icon fa-medium"><span class="label">Medium</span></a></li>
+			</ul>--> 
+		   </header>  
+		   <div class="container">
+		   		<h1>Numan</h1>
+		   		<?php include '../Cards/CARDs.php'; ?>
+		   </div>
+
+
+			<header id="header">
+				<a href="index.html" class="logo"><strong>Programming Books</strong></a>
+			</header>
+
+	</div>
+     </div>
+
+
+	<!-- Sidebar -->
+	 <div id="sidebar">
+	  <div class="inner">
+   	  <!-- Menu -->
+	<nav id="menu">
+     <header class="major">
+	   <h2>Coding Test</h2>
+	 </header>
+	 <ul>
+	  <li><a href="index.html">Home</a></li>
+	  <li><a href="HTML1.html">Coding Test</a></li>
+	  <li><a href="JS1.html">Programming Books</a></li>
+	  <li><a href="JS1.html">Coding Challenges</a></li>
+	  <li>
+		<span class="opener">More</span>
+	      <ul>
+			<li><a href="HTML1.html">LogIn</a></li>
+			<li><a href="CSS1.html">SignUp</a></li>
+			<li><a href="JS1.html">Admin Login</a></li>
+		  </ul>
+	  </li>
+	  <li><a href="About.html">About</a></li>
+	  <br><br>
+  
+
+
+							<!-- Section   (Contact Details) 
+								<section>
+									<header class="major">
+										<h2> </h2>
+									</header>
+									<p> </p>
+									<ul class="contact">
+										<li class="fa-envelope-o"><a href="#">mdnmnahmed@gmail.com</a></li>
+										<li class="fa-phone">+91 9967289433</li>
+										<li class="fa-home">Kokata,WB<br />
+										India</li>
+									</ul>
+								</section> -->
+	
+
+		</div>
+	   </div>
+		<!--Slide Bar End-->
+
+</div>
+
+		<!-- Scripts -->
+			<script src="js/jquery.min.js"></script>
+			<script src="js/browser.min.js"></script>
+			<script src="js/breakpoints.min.js"></script>
+			<script src="js/util.js"></script>
+			<script src="js/main.js"></script>
+
+	</body>
+</html>
